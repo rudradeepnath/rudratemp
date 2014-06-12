925d4a6536e28b353f9676e4aa195bd0d27b6a3b
rudradeepnath <rudradeepnath@sd.com>
2014-06-13 00:08:23 +0530

    commit

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+package com.javaranch.journal.cruisecontrol;
+
+/**
+ * The most unimaginative class one could come up with for demonstrating
+ * continuous integration.
+ * 
+ * @author Lasse Koskela
+ */
+public class Calculator {
+
+	public int add(int x, int y) {
+                return (x + y);
+	}
+
+	public int subtract(int x, int y) {
+		return (x - y);
+	}
+
+}
